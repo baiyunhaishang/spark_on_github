3c163e014cbb83e17d4213b5eff754cc7bce2871
SwordArtOnline <mitbaiyun@163.com>
2020-01-06 22:27:31 +0800

    gdg

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        HashMap hashMap = new HashMap();
+        hashMap.put("a", 1);
+        hashMap.put(1, 3);
+
+        System.out.println(hashMap);
+    }
+
+}
