82d1dcdd3dd78143c77bb690713845724bf667a5
baiyun <57526090+baiyunhaishang@users.noreply.github.com>
2020-01-06 22:15:56 +0800

    gdg

---
+++ src/main/scala/test1.scala
@@ -0,0 +1,15 @@
+/**
+ * @ClassName test1
+ * @Author xs
+ * @Date 2020年-01月-06日 18:52:47
+ * @Version V1.0
+ **/
+object test1 {
+  def main(args: Array[String]): Unit = {
+    println("helo git")
+    //ddd
+    println()
+    //d/ddds
+  }
+
+}
