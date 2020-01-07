7deab24deb7a6a05ab7a6cdf80cea4e1669567be
SwordArtOnline <mitbaiyun@163.com>
2020-01-07 12:24:18 +0800

    2020年1月7日12:24:12

---
+++ one/src/main/scala/test2.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkConf
+
+/**
+ * @ClassName test2
+ * @Author xs
+ * @Date 2020年-01月-07日 10:35:31
+ * @Version V1.0
+ **/
+object test2 {
+  def main(args: Array[String]): Unit = {
+    new SparkConf()
+  }
+
+}
